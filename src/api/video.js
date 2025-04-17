c72cbf953cbbdb53bcb513a62c7cedf9d009dfb1
ujjwal060 <ujjwal.singh@aayaninfotech.com>
2025-04-17 11:22:11 +0530

    add videos api in api component

--- src/api/video.js
+++ src/api/video.js
@@ -1,6 +1,16 @@
 import axios from "./axios";
 
+export const getVideos = async (data) => {
+  const response = await axios.post("/admin/video/getAll", data);
+  return response.data;
+};
+
 export const addVideos = async (data) => {
-  const response = await axios.post("/admin/forgote", data);
+  const response = await axios.post("/admin/video/add", data);
+  return response.data;
+};
+
+export const deleteVideos = async (data) => {
+  const response = await axios.post("/admin/video/deleteVideo", data);
   return response.data;
 };
