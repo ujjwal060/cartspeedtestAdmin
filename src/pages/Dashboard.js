ff6447b723c62a9de919f6358294cbe4d924a4c5
ujjwal060 <ujjwal.singh@aayaninfotech.com>
2025-04-22 19:23:06 +0530

    add chnage

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -36,14 +36,27 @@ const Dashboard = () => {
     labels: ["Delhi", "Mumbai", "Bangalore", "Chennai", "Kolkata"],
     datasets: [
       {
-        label: "Active Users",
+        label: "Users",
         data: [40, 60, 30, 20, 50],
-        backgroundColor: "rgba(63, 81, 181, 0.6)",
-        borderColor: "rgba(63, 81, 181, 1)",
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.6)", 
+          "rgba(54, 162, 235, 0.6)",
+          "rgba(255, 206, 86, 0.6)",
+          "rgba(75, 192, 192, 0.6)",
+          "rgba(153, 102, 255, 0.6)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+        ],
         borderWidth: 1,
       },
     ],
   };
+  
 
   return (
     <div className="container-fluid">
