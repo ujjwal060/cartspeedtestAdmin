5246e916977b70c3aa85971b49512f03c143ba13
daksh963655 <daksh.kumar@aayaninfotech.com>
2025-04-22 17:38:04 +0530

    update changes

--- src/pages/TestDashboard.js
+++ src/pages/TestDashboard.js
@@ -15,6 +15,7 @@ import "../components/css/accordion-level-styles.css";
 import FilterListIcon from "@mui/icons-material/FilterList";
 import LocationPinIcon from "@mui/icons-material/LocationPin";
 import "../index.css";
+import Loader from "../components/Loader";
 const TestDashboard = () => {
   const rowsPerPage = 10;
   const [currentPage, setCurrentPage] = useState(0);
@@ -24,6 +25,7 @@ const TestDashboard = () => {
   const [totalData, setTotalData] = useState([]);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  const [loading, setLoading] = useState(false);
   const [openFilter, setOpenFilter] = useState(false);
   const token = localStorage.getItem("token");
   const [level, setLevel] = useState("");
@@ -31,6 +33,7 @@ const TestDashboard = () => {
   const fetchQA = async () => {
     const offset = currentPage * rowsPerPage;
     const limit = rowsPerPage;
+    setLoading(true);
     try {
       const response = await getQA(token, offset, limit, filters);
       setGetData(response?.data);
@@ -38,14 +41,15 @@ const TestDashboard = () => {
     } catch (error) {
       console.error("Error fetching videos:", error);
     } finally {
+      setLoading(false);
     }
   };
   const handleChangePage = (_, newPage) => setCurrentPage(newPage);
 
   const handeOpenFilter = () => {
     setOpenFilter(!openFilter);
-    if(openFilter){
-      setFilters({})
+    if (openFilter) {
+      setFilters({});
     }
   };
 
@@ -57,7 +61,7 @@ const TestDashboard = () => {
 
   useEffect(() => {
     fetchQA();
-  }, [currentPage,filters]);
+  }, [currentPage, filters]);
 
   return (
     <Box p={4}>
@@ -117,66 +121,70 @@ const TestDashboard = () => {
           </Button>
         </div>
 
-        <Accordion
-          className="d-flex flex-column gap-3 custom-accordion"
-          flush
-        >
-          {getData?.map((item, index) => (
-            <Accordion.Item eventKey={index.toString()} key={item._id}>
-              <Accordion.Header
-                className={`accordion-button p-0 ${item.level?.toLowerCase()}`}
-              >
-                Q{currentPage * rowsPerPage + index + 1}. {item.question}
-              </Accordion.Header>
-              <Accordion.Body>
-                <div className="row align-items-start">
-                  <div className="col-lg-3">
-                    <div className="d-flex justify-content-start gap-1 align-items-center">
-                      <Typography className="text-end" variant="h6">
-                        Lv:
-                      </Typography>
-                      <span className="fs-6">{item.level}</span>
+        <Accordion className="d-flex flex-column gap-3 custom-accordion" flush>
+          {loading === true ? (
+            <div className="update-loader">
+              <Loader />
+            </div>
+          ) : (
+            getData?.map((item, index) => (
+              <Accordion.Item eventKey={index.toString()} key={item._id}>
+                <Accordion.Header
+                  className={`accordion-button p-0 ${item.level?.toLowerCase()}`}
+                >
+                  Q{currentPage * rowsPerPage + index + 1}. {item.question}
+                </Accordion.Header>
+                <Accordion.Body>
+                  <div className="row align-items-start">
+                    <div className="col-lg-3">
+                      <div className="d-flex justify-content-start gap-1 align-items-center">
+                        <Typography className="text-end" variant="h6">
+                          Lv:
+                        </Typography>
+                        <span className="fs-6">{item.level}</span>
+                      </div>
                     </div>
-                  </div>
-                  <div className="col-lg-9">
-                    <div className="d-flex justify-content-end gap-1 align-items-center">
-                      <Typography className="text-end" variant="h6">
-                        <LocationPinIcon />
-                      </Typography>
-                      <span className="fs-6">{item.state}</span>
+                    <div className="col-lg-9">
+                      <div className="d-flex justify-content-end gap-1 align-items-center">
+                        <Typography className="text-end" variant="h6">
+                          <LocationPinIcon />
+                        </Typography>
+                        <span className="fs-6">{item.state}</span>
+                      </div>
                     </div>
-                  </div>
 
-                  <div className="col-lg-6">
-                    <div className="row gy-3 align-items-center ps-1 pt-3">
-                      {item.options.map((option, optIndex) => (
-                        <div className="col-lg-6" key={option._id}>
-                          <Typography>
-                            {String.fromCharCode(65 + optIndex)}. {option.text}
-                          </Typography>
-                        </div>
-                      ))}
+                    <div className="col-lg-6">
+                      <div className="row gy-3 align-items-center ps-1 pt-3">
+                        {item.options.map((option, optIndex) => (
+                          <div className="col-lg-6" key={option._id}>
+                            <Typography>
+                              {String.fromCharCode(65 + optIndex)}.{" "}
+                              {option.text}
+                            </Typography>
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                    <div className="col-lg-6">
+                      <div className="mt-5 d-flex justify-content-end">
+                        {item.options.map((option, optIndex) =>
+                          option.isCorrect ? (
+                            <Chip
+                              key={option._id}
+                              label={`${String.fromCharCode(65 + optIndex)}. ${
+                                option.text
+                              }`}
+                              color="success"
+                            />
+                          ) : null
+                        )}
+                      </div>
                     </div>
                   </div>
-                  <div className="col-lg-6">
-                    <div className="mt-5 d-flex justify-content-end">
-                      {item.options.map((option, optIndex) =>
-                        option.isCorrect ? (
-                          <Chip
-                            key={option._id}
-                            label={`${String.fromCharCode(65 + optIndex)}. ${
-                              option.text
-                            }`}
-                            color="success"
-                          />
-                        ) : null
-                      )}
-                    </div>
-                  </div>
-                </div>
-              </Accordion.Body>
-            </Accordion.Item>
-          ))}
+                </Accordion.Body>
+              </Accordion.Item>
+            ))
+          )}
         </Accordion>
         <TablePagination
           rowsPerPageOptions={[rowsPerPage]}
