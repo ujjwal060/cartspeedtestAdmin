04389d4452034347a9e3d2b9d040bb7e0e6c351b
daksh963655 <daksh.kumar@aayaninfotech.com>
2025-07-17 10:42:26 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL:'http://52.20.55.193:9090/api',
+  baseURL:'http://98.82.228.18:9090/api',
   // baseURL:'http://localhost:9090/api',
   headers: {
     'Content-Type': 'application/json',
