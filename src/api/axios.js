2bd4197c20be6fb3fb5b7e46eb35ddd55a9f7f6c
ujjwal85 <ujjwalkumarsingh888@gmail.com>
2025-07-21 15:43:13 +0530

    add change

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL:'http://18.209.91.97:9090/api',
+  baseURL:'http://98.85.246.54:9090/api',
   // baseURL:'http://localhost:9090/api',
   headers: {
     'Content-Type': 'application/json',
